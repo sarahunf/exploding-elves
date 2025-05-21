06c379c22c1cc21b78a50b9dcbffc89b37425f93
Sarah Reis <reis.sarahtbr@gmail.com>
2025-05-21 18:40:08 -0300

    TMP pro + UI assets

---
+++ Exploding Elves/Assets/Scripts/Manager/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using Actors;
+using Actors.Enum;
+using Actors.Factory;
+using UnityEngine;
+namespace Manager
+{
+    public class GameManager : MonoBehaviour
+    {
+        [SerializeField] private Spawner.Spawner[] spawners;
+    
+        private void OnEnable()
+        {
+            Elf.OnElfReplication += HandleElfReplication;
+        }
+    
+        private void OnDisable()
+        {
+            Elf.OnElfReplication -= HandleElfReplication;
+        }
+    
+        private void HandleElfReplication(EntityType entityType, Vector3 position)
+        {
+            var factory = FindObjectOfType<ElfFactory>();
+            if (factory == null) return;
+            
+            var newElf = factory.CreateEntity(entityType);
+            newElf.Initialize(position);
+        }
+    
+        public void SetSpawnerInterval(int spawnerIndex, float interval)
+        {
+            if (spawnerIndex >= 0 && spawnerIndex < spawners.Length)
+            {
+                spawners[spawnerIndex].SetSpawnInterval(interval);
+            }
+        }
+    }
+
+}
---
+++ Exploding Elves/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,64 @@
+﻿using Manager;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace UI
+{
+    public class UIManager : MonoBehaviour
+    {
+        [System.Serializable]
+        public class SpawnerUI
+        {
+            public string spawnerName;
+            public Slider intervalSlider;
+            public TMP_Text intervalText;
+            public Image colorIndicator;
+        }
+    
+        [SerializeField] private SpawnerUI[] spawnerUIs;
+        [SerializeField] private GameManager gameManager;
+        [SerializeField] private Color[] elfColors; // Matched to EntityType enum order
+    
+        private void Start()
+        {
+            InitializeUI();
+        }
+    
+        private void InitializeUI()
+        {
+            if (gameManager == null)
+            {
+                gameManager = FindObjectOfType<GameManager>();
+                if (gameManager == null)
+                {
+                    Debug.LogError("GameManager not found in the scene!");
+                    return;
+                }
+            }
+        
+            for (int i = 0; i < spawnerUIs.Length; i++)
+            {
+                int spawnerIndex = i;
+                
+                UpdateIntervalText(spawnerUIs[i].intervalText, spawnerUIs[i].intervalSlider.value);
+                
+                if (i < elfColors.Length)
+                {
+                    spawnerUIs[i].colorIndicator.color = elfColors[i];
+                }
+                
+                spawnerUIs[i].intervalSlider.onValueChanged.AddListener((value) => 
+                {
+                    UpdateIntervalText(spawnerUIs[spawnerIndex].intervalText, value);
+                    gameManager.SetSpawnerInterval(spawnerIndex, value);
+                });
+            }
+        }
+    
+        private void UpdateIntervalText(TMP_Text text, float value)
+        {
+            text.text = value.ToString("F1") + "s";
+        }
+    }
+}
